db0b13bce0e0d6f971d2621b3a60d1ff58d494c3
ofichain <OFIChain@163.com>
2018-05-13 23:10:00 +0800

    051324

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -39,8 +39,8 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         ( 0, hashGenesisBlockOfficial )        
-	(1,   uint256("0x0000000af05e4ea4dd2391962eabc7be78d1c8d9eb7ba258dad105c30c7c7009"))
-	(2,   uint256("0x0000000ae78b915f692b55995b797fad1965c5416c53eb8ac2b00d80085005c7"))
+//	(1,   uint256("0x0000000af05e4ea4dd2391962eabc7be78d1c8d9eb7ba258dad105c30c7c7009"))
+//	(2,   uint256("0x0000000ae78b915f692b55995b797fad1965c5416c53eb8ac2b00d80085005c7"))
         //coingo.vip
 
         ;
